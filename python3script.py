7d3e86782596a328f9c22ea53dee280caf7f746d
Germán Corbetta <43816537+germancorbettamulesoft@users.noreply.github.com>
2022-11-03 16:56:54 -0300

    Create python3script.py

---
+++ python3script.py
@@ -0,0 +1,54 @@
+import os
+import csv
+
+# This information is relative to each Support Engineer
+directory = "/Users/gcorbetta/Downloads/cases"
+csvReport = "/Users/gcorbetta/Downloads/report.csv"
+closedTag = ".ready.to.be.closed"
+
+#Function to load my Cases from the local file system
+def loadMyCases():
+    folderArray = os.listdir(directory)
+    for folder in folderArray:
+        if(folder!=".DS_Store"):
+            myCases.append(folder)
+
+#Function to load the closed cases from the CSV file (UTF-8 required)
+#You may get the CSV by exporting the results of the "ClosedCasesForDeletingFolders" report (as UTF-8 CSV)
+def loadClosedCases():
+    with open(csvReport, encoding="utf8") as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",")
+        for row in csvreader:
+            try:
+                closedCases.append(row[0])
+            except IndentationError:
+                break
+            except IndexError:
+                break
+
+# Function to check arrays and identify folders to be closed
+def loadReadyToClose():
+    for myfolder in myCases:
+        if myfolder in closedCases:
+            readyToDelete.append(myfolder)
+
+#Main flow
+myCases = []
+closedCases = []
+readyToDelete = []
+loadMyCases()
+loadClosedCases()
+loadReadyToClose()
+
+print("*"*30)
+print("File System has: ", len(myCases), " folders")
+print("NA101 CSV file: ", len(closedCases), " folders")
+print("Ready to be marked for closure: ", len(readyToDelete), " folders")
+print("*"*30)
+
+# Rename the folder
+for case in readyToDelete:
+    oldCaseFolder = directory + "/" + case
+    newCaseFolder = oldCaseFolder + closedTag
+    if closedTag not in oldCaseFolder:
+        os.rename(oldCaseFolder, newCaseFolder)
