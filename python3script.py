ec05ec0db5361be3eab3f2a955a1a0caea2f2f96
Germán Corbetta <43816537+germancorbettamulesoft@users.noreply.github.com>
2022-11-03 17:17:17 -0300

    Added try-except into CSV report and cases folder

--- python3script.py
+++ python3script.py
@@ -8,23 +8,34 @@ closedTag = ".ready.to.be.closed"
 
 #Function to load my Cases from the local file system
 def loadMyCases():
-    folderArray = os.listdir(directory)
-    for folder in folderArray:
-        if(folder!=".DS_Store"):
-            myCases.append(folder)
+    try:
+        folderArray = os.listdir(directory)
+        for folder in folderArray:
+            if(folder!=".DS_Store"):
+                myCases.append(folder)
+    except FileNotFoundError:
+        print("Cases folder not found")
+        exit()
 
 #Function to load the closed cases from the CSV file (UTF-8 required)
 #You may get the CSV by exporting the results of the "ClosedCasesForDeletingFolders" report (as UTF-8 CSV)
 def loadClosedCases():
-    with open(csvReport, encoding="utf8") as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=",")
-        for row in csvreader:
-            try:
-                closedCases.append(row[0])
-            except IndentationError:
-                break
-            except IndexError:
-                break
+    try:
+        with open(csvReport, encoding="utf8") as csvfile:
+            csvreader = csv.reader(csvfile, delimiter=",")
+            for row in csvreader:
+                try:
+                    closedCases.append(row[0])
+                except IndentationError:
+                    break
+                except IndexError:
+                    break
+    except FileNotFoundError:
+        print("Unable to find CSV file")
+        exit()
+    except UnicodeDecodeError:
+        print("Unable to open CSV file, please check that is UTF-8 CSV")
+        exit()
 
 # Function to check arrays and identify folders to be closed
 def loadReadyToClose():
